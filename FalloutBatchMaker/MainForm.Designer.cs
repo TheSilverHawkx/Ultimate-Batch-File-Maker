3e4aaef2befec9be0044dbb7f1653cf3b8381ce0
Mike <omer.roth@ymail.com>
2017-10-29 07:43:39 +0200

    Base Version

--- FalloutBatchMaker/MainForm.Designer.cs
+++ FalloutBatchMaker/MainForm.Designer.cs
@@ -28,23 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.SuspendLayout();
             // 
-            // listBox1
+            // button1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 18);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(260, 225);
-            this.listBox1.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(388, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 100);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Location = new System.Drawing.Point(0, 150);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(495, 229);
+            this.tabControl1.TabIndex = 4;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(495, 379);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
             this.Name = "MainForm";
             this.Text = "Form2";
             this.ResumeLayout(false);
@@ -52,7 +65,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
---
+++ FalloutBatchMaker/MainForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+
+namespace FalloutBatchMaker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private IList<JObject> master_values;
+
+        private DataTable initializeDataTable(string[] columns, JArray values, string category)
+        {
+            DataTable dt = new DataTable();
+            foreach (var column in columns)
+            {
+                dt.Columns.Add(column);
+            }
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+
+            fd.Filter = "JSON file | *.json";
+            fd.Title = "Please select Resource JSON file";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                string json_file = System.IO.File.ReadAllText(fd.FileName);
+
+
+                JObject parsed_json = JObject.Parse(json_file);
+
+                if (parsed_json["Game"].Value<string>() == "Fallout 4")
+                {
+                    string category_name = parsed_json.Properties().Select(p => p.Name).ToList()[1];
+
+                    
+                    if (parsed_json[category_name].First().Count() != 2)
+                    {
+                        foreach (JObject child in parsed_json.Children().Last().Values())
+                        {
+                            string item_category = child.Properties().Select(p => p.Name).ToList()[0];
+
+                            foreach (JObject item in child.Children().First().Values())
+                            {
+                                item.Add("itemCategory", item_category);
+                                MessageBox.Show(item.ToString());
+                                master_values.Add(item);
+                            }                            
+                        }
+                        //DataTable dt = populateTable(master_values);
+                        //createTab(category_name, dt);
+                        master_values.Clear();
+                    }
+                    else
+                    {
+                        foreach (JObject child in parsed_json.Children().Last())
+                        {
+                            child.Add("itemCategory", category_name);
+
+                            //master_values.Add(JArray.Parse(child.ToString()));
+                        }
+                        //DataTable dt = populateTable(master_values);
+                        //createTab(category_name, dt);
+                        master_values.Clear();
+                    }                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not find Resource file. Exiting.");
+                this.Close();
+            }
+
+        }
+
+        private DataTable populateTable(JArray values)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Category");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Code");
+            dt.Columns.Add("Amount");
+
+
+            foreach (JObject row in values)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Category"] = row.Property("itemCategory").Value; ;
+                dr["Name"] = row.Property("name").Value;
+                dr["Code"] = row.Property("code").Value;
+                dr["Amount"] = 0;
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+
+        private void createTab(string category, DataTable dt)
+        {
+
+            TabPage tp = new TabPage(category);            
+            tabControl1.Controls.Add(tp);
+
+            DataGridView dgv = new DataGridView();
+            dgv.DataSource = "";
+            dgv.Dock = DockStyle.Fill;
+            dgv.Name = category + "DGView";
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv.DataSource = dt;
+            tp.Controls.Add(dgv);
+
+
+
+            BindingNavigator bn = new BindingNavigator();
+            ToolStripSeparator tsp = new ToolStripSeparator();
+            ToolStripLabel tsl1 = new ToolStripLabel("List Count:");
+            ToolStripLabel tsl1c = new ToolStripLabel("100");
+            tsl1c.Name = category + "total_items_lbl";
+            ToolStripLabel tsl2 = new ToolStripLabel("Selected Items:");
+            ToolStripLabel tsl2c = new ToolStripLabel("100");
+            tsl2c.Name = category + "total_items_lbl";
+            ToolStripTextBox tstxtbx = new ToolStripTextBox(category + "search_txtbx");
+            tstxtbx.Alignment = ToolStripItemAlignment.Right;
+            ToolStripButton tsbtn = new ToolStripButton("Search");
+            tsbtn.Name = category + " search_btn";
+            tsbtn.Alignment = ToolStripItemAlignment.Right;
+
+            bn.Items.Add(tsl1);
+            bn.Items.Add(tsl1c);
+            bn.Items.Add(tsp);
+            bn.Items.Add(tsl2);
+            bn.Items.Add(tsl2c);
+            bn.Items.Add(tsbtn);
+            bn.Items.Add(tstxtbx);
+            bn.Dock = DockStyle.Top;
+            tp.Controls.Add(bn);
+        }
+    }
+
+    
+}
