a458bbd1dc4f4f549715218e25142a0d9ac5b6ea
The Silver Hawk <omer.roth@ymail.com>
2017-11-01 23:21:12 +0200

    Add files via upload
    script to add resources

---
+++ FalloutBatchMaker/Untitled1.ps1
@@ -0,0 +1,23 @@
+﻿
+$path = read-host "Enter Path"
+$content = Get-Content $path
+
+$i = 0
+foreach ($line in $content)
+{
+
+    if ($line -like "*=*")
+    {
+        $name = $($line -split " = ")[0]
+        $code = $($line -split " = ")[1]
+        $output = "  $title[$i]`n    name = " + $name + "`n    code = " + $code
+        $i++
+        write-host $output
+        
+    }
+    else {
+        $title = $line
+        $i = 0
+        
+    }    
+}
